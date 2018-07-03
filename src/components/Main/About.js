790bba7f8b1d844de64cfe6c682e441c556e708b
LP Boucher <lp_boucher@hotmail.com>
2018-07-03 23:54:33 +0200

    added overdrive page animation

--- src/components/Main/About.js
+++ src/components/Main/About.js
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
+import { Transition } from 'react-transition-group';
+import Overdrive from 'react-overdrive';
 
 import { AboutContainer } from '../Styles/MainStyles';
 
-const About = ({ about }) => (
-  <AboutContainer>
-    {/* <h1>{about.title}</h1>
-    <hr />
-<h3>{about.location}</h3> */}
-    <p>{about.body.body}</p>
-  </AboutContainer>
-);
+class About extends Component {
+  render() {
+    const { about, location } = this.props;
+    return (
+      <AboutContainer >
+        <p>{about.body.body}</p>
+        <Overdrive to="logoAnimation">
+          <p>velo creative studio</p>
+        </Overdrive>
+      </AboutContainer>
+    );
+  }
+}
 
 About.propTypes = {
   about: PropTypes.shape({
