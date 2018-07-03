43dae7813f6f8556b44615a0525b75a3beaa69f5
LP Boucher <lp_boucher@hotmail.com>
2018-07-03 23:53:55 +0200

    changed to stateless component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,21 +1,18 @@
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
+import Overdrive from 'react-overdrive';
 
-import { HeaderWrapper, HeaderContainer, StyledLink } from '../Styles/HeaderStyles';
+import { HeaderWrapper, HeaderContainer, StyledLogo } from '../Styles/HeaderStyles';
 import MainNav from './MainNav';
 
-class Header extends Component {
-  render() {
-    const { data, location } = this.props;
-    return (
-      <HeaderWrapper>
-        <HeaderContainer>
-          <StyledLink to="/">velo creative studio</StyledLink>
-          <MainNav nav={data.allContentfulNavigation.edges} />
-        </HeaderContainer>
-      </HeaderWrapper>
-    );
-  }
-}
+const Header = ({ nav, location }) => (
+  <HeaderWrapper>
+    <HeaderContainer>
+      <Overdrive to="logoAnimation">
+        <StyledLogo location={location} to="/">velo creative studio</StyledLogo>
+      </Overdrive>
+      <MainNav nav={nav.allContentfulNavigation.edges} />
+    </HeaderContainer>
+  </HeaderWrapper>
+);
 
 export default Header;
