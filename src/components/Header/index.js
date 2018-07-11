ffd800e64c46568faada813403e65c917cc92bb7
LP Boucher <lp_boucher@hotmail.com>
2018-07-11 23:31:45 +0200

    fixed overdrive reference id

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -7,7 +7,7 @@ import MainNav from './MainNav';
 const Header = ({ nav, location }) => (
   <HeaderWrapper>
     <HeaderContainer>
-      <Overdrive to="logoAnimation">
+      <Overdrive id="logoAnimation">
         <StyledLogo location={location} to="/">velo creative studio</StyledLogo>
       </Overdrive>
       <MainNav nav={nav.allContentfulNavigation.edges} />
