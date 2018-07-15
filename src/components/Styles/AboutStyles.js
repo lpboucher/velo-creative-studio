68d59c8696c4619425a9452967dd4ac9d50046dc
LP Boucher <lp_boucher@hotmail.com>
2018-07-15 10:59:52 +0200

    reduced text size on about page, also made feature text lighter

--- src/components/Styles/AboutStyles.js
+++ src/components/Styles/AboutStyles.js
@@ -4,6 +4,7 @@ import styled from 'styled-components';
 import { Intro } from '../Styles/MainStyles';
 
 export const AboutIntro = Intro.extend`
+    font-size: 16px;
 `;
 
 export const FeatureWrapper = styled.div`
@@ -18,14 +19,18 @@ export const AboutTitle = styled.div`
     position: absolute;
     top: 40%;
     left: 60%;
-    font-size: 40px;
+    font-size: 36px;
+    font-weight: 300;
     line-height: 1.5;
+    padding-right: 20px;
 `;
 
 export const AboutProfile = Intro.extend`
     padding-right: 40px;
+    font-size: 16px;
 `;
 
 export const AboutCollab = Intro.extend`
     padding-right: 40px;
+    font-size: 16px;
 `;
