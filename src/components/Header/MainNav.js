aa9d84ec8a1d4e63611ee2d304c684d51647abf1
LP Boucher <lp_boucher@hotmail.com>
2018-07-29 16:49:49 +0200

    changed MainNav component to class-based, add initial state and state methods to handle toggling of mobile menu

--- src/components/Header/MainNav.js
+++ src/components/Header/MainNav.js
@@ -1,20 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Link from 'gatsby-link';
 
 import { StyledNavList, MobileIcon } from '../Styles/HeaderStyles';
 import NavItem from './NavItem';
 
-const MainNav = ({ nav }) => (
-  <nav>
-    <MobileIcon href="#">☰</MobileIcon>
-    <StyledNavList>
-      {nav.map(({ node }) => (
-        <NavItem key={node.id} item={node} />
-    ))}
-    </StyledNavList>
-  </nav>
-);
+class MainNav extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isHidden: true,
+    };
+    this.toggleHidden = this.toggleHidden.bind(this);
+    this.resetHidden = this.resetHidden.bind(this);
+  }
+
+  toggleHidden() {
+    this.setState({
+      isHidden: !this.state.isHidden,
+    });
+  }
+
+  resetHidden() {
+    this.setState({
+      isHidden: true,
+    });
+  }
+
+  render() {
+    return (
+      <nav>
+        <MobileIcon href="#" onClick={this.toggleHidden}>☰</MobileIcon>
+        <StyledNavList hide={this.state.isHidden}>
+          {this.props.nav.map(({ node }) => (
+            <NavItem key={node.id} item={node} reset={this.resetHidden} />
+        ))}
+        </StyledNavList>
+      </nav>
+    );
+  }
+}
 
 export default MainNav;
 
