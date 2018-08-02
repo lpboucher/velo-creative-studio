04cbd0470359cf36c740e10c98e8580bdf3fea74
LP Boucher <lp_boucher@hotmail.com>
2018-08-02 22:09:49 +0200

    made parent Header container into class-based component to allow for state to be available in more children

--- src/components/Header/MainNav.js
+++ src/components/Header/MainNav.js
@@ -5,41 +5,18 @@ import Link from 'gatsby-link';
 import { StyledNavList, MobileIcon } from '../Styles/HeaderStyles';
 import NavItem from './NavItem';
 
-class MainNav extends Component {
-  constructor() {
-    super();
-    this.state = {
-      isHidden: true,
-    };
-    this.toggleHidden = this.toggleHidden.bind(this);
-    this.resetHidden = this.resetHidden.bind(this);
-  }
-
-  toggleHidden() {
-    this.setState({
-      isHidden: !this.state.isHidden,
-    });
-  }
-
-  resetHidden() {
-    this.setState({
-      isHidden: true,
-    });
-  }
-
-  render() {
-    return (
-      <nav>
-        <MobileIcon href="#" onClick={this.toggleHidden}>☰</MobileIcon>
-        <StyledNavList hide={this.state.isHidden}>
-          {this.props.nav.map(({ node }) => (
-            <NavItem key={node.id} item={node} reset={this.resetHidden} />
-        ))}
-        </StyledNavList>
-      </nav>
-    );
-  }
-}
+const MainNav = ({
+  nav, toggleHidden, isHidden, resetHidden,
+}) => (
+  <nav>
+    <MobileIcon href="#" onClick={toggleHidden}>☰</MobileIcon>
+    <StyledNavList hide={isHidden}>
+      {nav.map(({ node }) => (
+        <NavItem key={node.id} item={node} reset={resetHidden} />
+    ))}
+    </StyledNavList>
+  </nav>
+);
 
 export default MainNav;
 
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,15 +4,45 @@ import Overdrive from 'react-overdrive';
 import { HeaderWrapper, HeaderContainer, StyledLogo } from '../Styles/HeaderStyles';
 import MainNav from './MainNav';
 
-const Header = ({ nav, location }) => (
-  <HeaderWrapper>
-    <HeaderContainer>
-      <Overdrive id="logoAnimation">
-        <StyledLogo location={location} to="/">velo creative studio</StyledLogo>
-      </Overdrive>
-      <MainNav nav={nav.edges} />
-    </HeaderContainer>
-  </HeaderWrapper>
-);
+class Header extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isHidden: true,
+    };
+    this.toggleHidden = this.toggleHidden.bind(this);
+    this.resetHidden = this.resetHidden.bind(this);
+  }
+
+  toggleHidden() {
+    this.setState({
+      isHidden: !this.state.isHidden,
+    });
+  }
+
+  resetHidden() {
+    this.setState({
+      isHidden: true,
+    });
+  }
+
+  render() {
+    return (
+      <HeaderWrapper>
+        <HeaderContainer>
+          <Overdrive id="logoAnimation">
+            <StyledLogo location={location} onClick={this.resetHidden} to="/">velo creative studio</StyledLogo>
+          </Overdrive>
+          <MainNav
+            nav={this.props.nav.edges}
+            toggleHidden={this.toggleHidden}
+            isHidden={this.state.isHidden}
+            resetHidden={this.resetHidden}
+          />
+        </HeaderContainer>
+      </HeaderWrapper>
+    );
+  }
+}
 
 export default Header;
