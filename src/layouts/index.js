d36f3098d786d80c2270faa53531846c9bc0209a
LP Boucher <lp_boucher@hotmail.com>
2018-08-05 22:35:01 +0200

    required, and instantiated IntlProvider as wrapper to whole website to pass down locale selected

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
+import { getCurrentLangKey, getLangs, getUrlForLang } from 'ptz-i18n';
+import { IntlProvider, addLocaleData } from 'react-intl';
+import 'intl';
 
 import Header from '../components/Header';
 import Footer from '../components/Footer/Footer';
@@ -8,29 +11,51 @@ import './index.css';
 
 import logo from '../images/favicon.ico';
 
-const TemplateWrapper = ({ children, data, location }) => (
-  <div>
-    <Helmet
-      title={data.contentfulAbout.seoTitle}
-      meta={[
-        { name: 'description', content: data.contentfulAbout.seoDescription.seoDescription },
-      ]}
-      link={[
-        { rel: 'shortcut icon', type: 'image/png', href: `${logo}` },
-    ]}
-    />
-    <Header nav={data.allContentfulNavigation} location={location} />
-    {children()}
-    <Footer footer={data.allContentfulFooterItem} />
-  </div>
-);
+import en from 'react-intl/locale-data/en';
+import 'intl/locale-data/jsonp/en';
+import fr from 'react-intl/locale-data/fr';
+import 'intl/locale-data/jsonp/fr';
+
+addLocaleData([...en, ...fr]);
+
+const TemplateWrapper = ({ children, data, location }) => {
+  const url = location.pathname;
+  const { langs, defaultLangKey } = data.site.siteMetadata.languages;
+  const langKey = getCurrentLangKey(langs, defaultLangKey, url);
+  const homeLink = `/${langKey}/`;
+  const langsMenu = getLangs(langs, langKey, getUrlForLang(homeLink, url));
+
+  return (
+    <IntlProvider locale={langKey}>
+      <div>
+        <Helmet
+          title={data.contentfulAbout.seoTitle}
+          meta={[
+            { name: 'description', content: data.contentfulAbout.seoDescription.seoDescription },
+          ]}
+          link={[
+            { rel: 'shortcut icon', type: 'image/png', href: `${logo}` },
+          ]}
+        />
+        <Header
+          nav={data.allContentfulNavigation}
+          location={location}
+          langs={langsMenu}
+          locale={langKey}
+        />
+        {children()}
+        <Footer footer={data.allContentfulFooterItem} locale={langKey} />
+      </div>
+    </IntlProvider>
+  );
+};
 
 TemplateWrapper.propTypes = {
   children: PropTypes.func,
 };
 
 export const query = graphql`
-  query TemplateQuery{
+  query TemplateQuery {
     allContentfulNavigation (
       filter: {visible: {eq:true}}
       sort: {fields:[order]}
@@ -38,11 +63,14 @@ export const query = graphql`
       edges {
         node {
           ...NavData
+          node_locale
         }
       }
     }
     allContentfulFooterItem (
-      filter: {visible: {eq:true}}
+      filter: {visible: {eq:true}
+              node_locale: {eq:"en-US"}                   
+    }
       sort: {fields:[order]}
     ) {
       edges {
@@ -55,6 +83,15 @@ export const query = graphql`
       seoTitle
       seoDescription {
       seoDescription
+      }
+    }
+    site {
+      siteMetadata {
+        title
+        languages {
+          langs
+          defaultLangKey
+        }
       }
     }
   }
