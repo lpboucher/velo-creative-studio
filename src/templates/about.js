479b0d4842fcf438d139d83d81450b119ef53bbe
LP Boucher <lp_boucher@hotmail.com>
2018-08-21 00:35:54 +0200

    abstracted away instagram component, also changed about component to stateless function

--- src/templates/about.js
+++ src/templates/about.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import Img from 'gatsby-image';
-import Instafeed from 'react-instafeed';
 
-import { InstaContainer, LargeColumn, SmallColumn } from '../components/Styles/MainStyles';
+import Instagram from '../components/Main/Instagram';
+
+import { LargeColumn, SmallColumn } from '../components/Styles/MainStyles';
 import {
   AboutIntro,
   FeatureWrapper,
@@ -13,51 +14,29 @@ import {
   Testimonial,
 } from '../components/Styles/AboutStyles';
 
-class about extends Component {
-  render() {
-    const instafeedTarget = 'instafeed';
-    const { data } = this.props;
-
-    return (
-      <div>
-        <AboutIntro>{data.contentfulAbout.aboutPageTop.aboutPageTop}</AboutIntro>
-        <FeatureWrapper>
-          <LargeColumn>
-            <Img sizes={data.contentfulAbout.aboutPageFeature.sizes} alt={data.contentfulAbout.aboutPageFeature.description} />
-          </LargeColumn>
-          <SmallColumn>
-            <AboutTitle>{data.contentfulAbout.aboutPageTitle}</AboutTitle>
-          </SmallColumn>
-        </FeatureWrapper>
-        <AboutProfile>{data.contentfulAbout.aboutPageProfile.aboutPageProfile}</AboutProfile>
-        <AboutHeader>{data.contentfulAbout.aboutPageDevTitle}</AboutHeader>
-        <AboutCollab>{data.contentfulAbout.aboutPageCollaborator.aboutPageCollaborator}</AboutCollab>
-        <Testimonial>
-          {/* data.contentfulTestimonial.map((testimonial, index) => ( */
-            <p>{`${data.contentfulTestimonial.quote.quote} - ${data.contentfulTestimonial.clientName}, ${data.contentfulTestimonial.organisation}`}</p>
+const about = ({ data }) => (
+  <div>
+    <AboutIntro>{data.contentfulAbout.aboutPageTop.aboutPageTop}</AboutIntro>
+    <FeatureWrapper>
+      <LargeColumn>
+        <Img sizes={data.contentfulAbout.aboutPageFeature.sizes} alt={data.contentfulAbout.aboutPageFeature.description} />
+      </LargeColumn>
+      <SmallColumn>
+        <AboutTitle>{data.contentfulAbout.aboutPageTitle}</AboutTitle>
+      </SmallColumn>
+    </FeatureWrapper>
+    <AboutProfile>{data.contentfulAbout.aboutPageProfile.aboutPageProfile}</AboutProfile>
+    <AboutHeader>{data.contentfulAbout.aboutPageDevTitle}</AboutHeader>
+    <AboutCollab>{data.contentfulAbout.aboutPageCollaborator.aboutPageCollaborator}</AboutCollab>
+    <Testimonial>
+      {/* data.contentfulTestimonial.map((testimonial, index) => ( */
+        <p>{`${data.contentfulTestimonial.quote.quote} - ${data.contentfulTestimonial.clientName}, ${data.contentfulTestimonial.organisation}`}</p>
           /* )) */ }
-        </Testimonial>
-        <AboutHeader>@vero.lagarde</AboutHeader>
-        <InstaContainer id={instafeedTarget}>
-          <Instafeed
-            limit="6"
-            ref="instafeed"
-            resolution="low_resolution"
-            sortBy="most-recent"
-            target={instafeedTarget}
-            template="
-              <a href='{{link}}' target='_blank' class='instafeed__item'>
-                <img class='instafeed__item__background' src='{{image}}' />
-               </a>"
-            userId={`${process.env.GATSBY_INSTAGRAM_USER_ID}`}
-            clientId={`${process.env.GATSBY_INSTAGRAM_CLIENT_ID}`}
-            accessToken={`${process.env.GATSBY_INSTAGRAM_ACCESS_TOKEN}`}
-          />
-        </InstaContainer>
-      </div>
-    );
-  }
-}
+    </Testimonial>
+    <AboutHeader>@vero.lagarde</AboutHeader>
+    <Instagram />
+  </div>
+);
 
 export const query = graphql`
 query AboutPageTest($locale: String!) {
