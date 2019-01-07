ca7cdd4955284d4d12e8599b0b29d733a458d0f1
LP Boucher <lp_boucher@hotmail.com>
2019-01-07 22:23:11 +0100

    added Testimonial fragment reflecting props required

--- src/components/Main/Testimonial.js
+++ src/components/Main/Testimonial.js
@@ -3,11 +3,13 @@ import PropTypes from 'prop-types';
 
 import { TestimonialContainer } from '../Styles/AboutStyles';
 
-const Testimonial = ({ clientName, organisation, quote }) => (
+const Testimonial = ({
+  clientName,
+  organisation,
+  quote,
+}) => (
   <TestimonialContainer>
-    {/* data.contentfulTestimonial.map((testimonial, index) => ( */
-      <p>{`${quote.quote} - ${clientName}, ${organisation}`}</p>
-        /* )) */ }
+    <p>{`${quote} - ${clientName}, ${organisation}`}</p>
   </TestimonialContainer>
 );
 
@@ -23,5 +25,16 @@ Testimonial.propTypes = {
     quote: PropTypes.string,
   }).isRequired,
 };
+
+export const query = graphql`
+  fragment TestimonialData on ContentfulTestimonial {
+    clientName
+    organisation
+    quote {
+      id
+      quote
+    }
+  }
+`;
 
 export default Testimonial;
