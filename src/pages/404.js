8dd6b9afd33df69ff196be748cbdf8816149e5e9
LP Boucher <lp_boucher@hotmail.com>
2019-01-22 21:15:14 +0100

    fixed styles object

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const NotFoundPage = () => (
   <div style={{
- height: '90vh', 'background-color': '#f2f2f0', 'text-align': 'center', padding: '60px',
+ height: '90vh', backgroundColor: '#f2f2f0', 'text-align': 'center', padding: '60px',
 }}>
     <h1>NOT FOUND</h1>
     <p>The page you were looking for does not exist.</p>
